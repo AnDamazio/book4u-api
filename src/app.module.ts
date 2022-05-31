83c529073ab5f0814f4e0c131cd1fa4994ea0335
AnDamazio <damazioandre@hotmail.com>
2022-05-31 09:12:04 -0300

    Add category controller, add list category route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,34 @@
-import { AutoRelationBooksServicesModule } from './service/use-cases/autoRelationBooks/autoRelationBooks.module';
-import { PersonalDataController } from './controllers/personal-data.controller';
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserController } from './controllers/user.controller';
+import { AutoRelationBooksServicesModule } from "./service/use-cases/autoRelationBooks/autoRelationBooks.module";
+import { PersonalDataController } from "./controllers/personal-data.controller";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserController } from "./controllers/user.controller";
 import {
   AutoRelationBooksServices,
   BookCategoriesServices,
   PersonalDataServices,
   UserServices,
-} from './service';
-import { BookCategoriesServicesModule } from './service/use-cases/book-categories/book-categories-services.module';
-import { DataServicesModule } from './service/data-services';
-import { PersonalDataServicesModule } from './service/use-cases/personal-data/personal-data-service.module';
-import { UserServicesModule } from './service/use-cases/user/user-services.module';
-import { AuthModule } from './frameworks/auth/auth.module';
-import { BookController } from './controllers/book.controller';
-import { BookServices } from './service/use-cases/book/book-services.service';
-import { BookServicesModule } from './service/use-cases/book/book-services.module';
-import { AuthorServicesModule } from './service/use-cases/author';
-import { LanguageServicesModule } from './service/use-cases/language';
-import { PublisherServicesModule } from './service/use-cases/publisher/publisher-services.module';
-import { CategoryServicesModule } from './service/use-cases/category';
-import { UserSituationServicesModule } from './service/use-cases/userSituation';
-import { BookImagesServicesModule } from './service/use-cases/bookImages';
-import { TokenController } from './controllers/token.controller';
-import { WishListServices } from './service/use-cases/wish-list';
-import { WishListController } from './controllers/wish-list.controller';
-import { WishListServicesModule } from './service/use-cases/wish-list/wish-list-services.module';
+} from "./service";
+import { BookCategoriesServicesModule } from "./service/use-cases/book-categories/book-categories-services.module";
+import { DataServicesModule } from "./service/data-services";
+import { PersonalDataServicesModule } from "./service/use-cases/personal-data/personal-data-service.module";
+import { UserServicesModule } from "./service/use-cases/user/user-services.module";
+import { AuthModule } from "./frameworks/auth/auth.module";
+import { BookController } from "./controllers/book.controller";
+import { BookServices } from "./service/use-cases/book/book-services.service";
+import { BookServicesModule } from "./service/use-cases/book/book-services.module";
+import { AuthorServicesModule } from "./service/use-cases/author";
+import { LanguageServicesModule } from "./service/use-cases/language";
+import { PublisherServicesModule } from "./service/use-cases/publisher/publisher-services.module";
+import { CategoryServices, CategoryServicesModule } from "./service/use-cases/category";
+import { UserSituationServicesModule } from "./service/use-cases/userSituation";
+import { BookImagesServicesModule } from "./service/use-cases/bookImages";
+import { TokenController } from "./controllers/token.controller";
+import { WishListServices } from "./service/use-cases/wish-list";
+import { WishListController } from "./controllers/wish-list.controller";
+import { WishListServicesModule } from "./service/use-cases/wish-list/wish-list-services.module";
+import { CategoryController } from "./controllers/category.controller";
 
 @Module({
   imports: [
@@ -45,6 +46,7 @@ import { WishListServicesModule } from './service/use-cases/wish-list/wish-list-
     AuthModule,
     AutoRelationBooksServicesModule,
     BookCategoriesServicesModule,
+    CategoryServicesModule,
   ],
   controllers: [
     AppController,
@@ -53,6 +55,7 @@ import { WishListServicesModule } from './service/use-cases/wish-list/wish-list-
     TokenController,
     PersonalDataController,
     WishListController,
+    CategoryController,
   ],
   providers: [
     AppService,
@@ -64,6 +67,7 @@ import { WishListServicesModule } from './service/use-cases/wish-list/wish-list-
     BookServices,
     AutoRelationBooksServices,
     BookCategoriesServices,
+    CategoryServices
   ],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { IDataServices } from "src/core";
+import { JwtAuthGuard } from "src/frameworks/auth/jwt-auth.guard";
+
+@Controller("api/category")
+@UseGuards(JwtAuthGuard)
+export class CategoryController {
+  constructor(private categoryServices: IDataServices) {}
+
+  @Get("list-all")
+  async listCategories() {
+    return await this.categoryServices.category.findAll();
+  }
+}
