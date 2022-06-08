7410bc04c1d0f42bb3d1d3cf171f0ca7f7c0d31b
Gustavo Fontana Vieira <83253065+gustavofontanavieira@users.noreply.github.com>
2022-06-08 16:09:49 -0300

    some fixes in tbl_request and exchangeHistory

--- src/core/abstracts/request-repository.abstract.ts
+++ src/core/abstracts/request-repository.abstract.ts
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import { UpdateResult } from "typeorm";
 
 export abstract class IRequestRepository<T> {
-    abstract createExchangeBooks(book: T): Promise<T>
+  abstract createExchangeBooks(book: T): Promise<T>
 
-    abstract updateExchangeBooks(id: number, book: T): Promise<UpdateResult>
+  abstract updateExchangeBooks(id: number, book: T): Promise<UpdateResult>
 
-    abstract exchangeNotification(token: string): Promise<T>
+  abstract exchangeNotification(token: string): Promise<T>
 
-    abstract getIdFromExchangeBook(exchange: T): Promise<T>
+  abstract getIdFromExchangeBook(exchange: T): Promise<T>
 
-    abstract findExchangeById(id: number): Promise<T>
+  abstract findExchangeById(id: number): Promise<T>
 
 }
-=======
-export abstract class IRequestRepository<T> {
-  abstract create(request: T): Promise<T>;
-
-  abstract findAllRequest(id: string): Promise<T[]>;
-
-  abstract deleteRequest(id: string): Promise<any>;
-}
->>>>>>> 0587f5a94a366ffcb70a7556f72868f380c5b4be
--- src/core/entities/request.entity.ts
+++ src/core/entities/request.entity.ts
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 import { CreateBookDto } from "../dtos";
 import { ExchangeSituation } from "../enums";
 
 export class Request {
-    book1: CreateBookDto;
-    book2: CreateBookDto;
-    situation: ExchangeSituation;
-    createdAt: string;
+  book1: CreateBookDto;
+  book2: CreateBookDto;
+  situation: ExchangeSituation;
+  createdAt: string;
 }
-=======
-import { ExchangeHistory } from "./exchange-history.entity";
-import { User } from "./user.entity";
-
-export class Request {
-  situation: string;
-  offeredBook: string;
-  availableBook: string;
-  user: User[];
-  exchangeHistory: ExchangeHistory;
-}
->>>>>>> 0587f5a94a366ffcb70a7556f72868f380c5b4be
