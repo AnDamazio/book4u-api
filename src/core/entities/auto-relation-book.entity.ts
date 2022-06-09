e970c70dd06d36fde255fef360a30cdeada72314
AnDamazio <damazioandre@hotmail.com>
2022-06-09 01:27:04 -0300

    Small fixes

--- src/core/entities/auto-relation-book.entity.ts
+++
@@ -1,9 +0,0 @@
-import { CreateBookDto } from "../dtos";
-import { ExchangeSituation } from "../enums";
-
-export class AutoRelationBook {
-    book1: CreateBookDto;
-    book2: CreateBookDto;
-    situation: ExchangeSituation;
-    createdAt: string;
-}
